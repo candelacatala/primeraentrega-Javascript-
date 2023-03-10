ebdf66ac8ba25a3938aeaffb516080c6776b7f60
Candela Catala <candelacatala@hotmail.com>
2023-03-09 21:05:49 -0300

    first commit

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
-let nombre = prompt("Ingresa tu nombre");
-function saludar(){
-   alert ("Hola" + nombre);
+const usuario = {
+    nombre: prompt("Ingresa tu nombre"),
+    Nacionalidad: prompt("Ingresa tu nacionalidad"),
+    saludar
 }
+function saludar(){
+   alert ("Hola" + " " + this.nombre);
+}
+usuario.saludar();
+
+
+let email = prompt("Ingresa tu email");
+let password = prompt("Ingresa una contraseña");
+
+function usuarioNuevo(email,contraseña){
+    this.email = email,
+    this.password = password
+}
+
+let usuario1 = new usuarioNuevo(email, password)
+console.log(usuario1);
+
+const productos = [
+  { nombre: "Tops", precio: 4500 },
+  { nombre: "Bombachas", precio: 3500 },
+  { nombre: "Pilusos", precio: 3000 }
+];
+
+let seleccionProducto = prompt("Que estabas buscando? bombachas, tops o pilusos?");
+alert( productos.filter((producto) => producto.nombre === seleccionProducto));
+
+
 function calcular(){
     var valorFinal
     let total = parseInt(prompt("Ingresa el total de tu compra"));
